02666759c703775a9e53af504126887fa0d3d7dd
me@linux <me@linux>
2022-10-18 22:46:50 +0800

    init

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "hello world")
 	})
-	err := http.ListenAndServe(":8080",nil)
+	err := http.ListenAndServe("0.0.0.0:8080", nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
