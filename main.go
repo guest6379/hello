815a17146944fcf0fff13c184c730ac8f34a2e9a
me@linux <me@linux>
2022-10-18 22:55:37 +0800

    init

--- main.go
+++ main.go
@@ -4,13 +4,19 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "hello world")
 	})
-	err := http.ListenAndServe("0.0.0.0:8080", nil)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	err := http.ListenAndServe(fmt.Sprintf(":%v", port), nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
